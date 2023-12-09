dcaaaf6447a105aeb7cebdedde974a6bae240f85
Piotr Martyński <p.martynski@gmail.com>
2023-12-09 01:10:35 +0100

    Handle control bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useRef, useState } from 'react'
 import './App.css'
 import Board from './components/Board'
 
@@ -99,26 +99,64 @@ function iterateState(boardState: boolean[]) : boolean[] {
 }
 
 function App() {
-  const [cells, setCells] = useState(cleanCells());
+  const cells = useRef(cleanCells());
+  const [updateTrigger, setUpdateTrigger] = useState(false);
+  const [running, setRunning] = useState(false);
+  const [tickCount, setTickCount] = useState(0);
+
+  useEffect(() => {
+    if(running) {
+      const timer = setTimeout(() => {
+        cells.current = iterateState(cells.current);
+        setTickCount(tickCount + 1);
+      }, 1);
+      return () => clearTimeout(timer);
+    }
+    return undefined;
+  }, [running, tickCount])
   
   const toggleCell = (index: number) => {
-    const newState = cells.slice(0);
-    newState[index] = !newState[index];
-    setCells(newState);
+    cells.current[index] = !cells.current[index];
   };
+
+  const handleRand = () => {
+    if (running) {
+      return;
+    }
+
+    setUpdateTrigger(!updateTrigger);
+    cells.current = randomizeCells();
+  }
+
+  const handleClean = () => {
+    if (running) {
+      return;
+    }
+
+    setUpdateTrigger(!updateTrigger);
+    cells.current = cleanCells();
+  }
+
+  const handleStartStop = () => {
+    if(running) {
+      setTickCount(0);
+    }
+  
+    setRunning(!running);
+  }
 
   return (
     <>
       <div>
-        <BoardContext.Provider value={{ cells, onClick: toggleCell}}>
+        <BoardContext.Provider value={{ cells: cells.current, onClick: toggleCell }}>
           <div className="controls">
-            <button onClick={() => setCells(iterateState(cells))}>Start</button>
-            <button onClick={() => setCells(randomizeCells())}>Rand</button>
-            <button onClick={() => setCells(cleanCells)}>Clean</button>
+            <button onClick={handleStartStop}>{running ? 'Stop' : 'Start'}</button>
+            <button onClick={handleRand}>Rand</button>
+            <button onClick={handleClean}>Clean</button>
             <span className="label">Ticks:</span>
-            <span className="display" id="ticks">0</span>
+            <span className="display" id="ticks">{tickCount}</span>
             <span className="label">Alive:</span>
-            <span className="display" id="alive">0</span>
+            <span className="display" id="alive">{cells.current.filter(c => c).length}</span>
           </div>
           <Board />
         </BoardContext.Provider>
